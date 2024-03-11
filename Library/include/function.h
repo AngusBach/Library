c3a4ce826b1ce9ed422844246b61095ed1531c90
AngusBach <156666508+AngusBach@users.noreply.github.com>
2024-03-11 11:18:46 +0100

    dgeg
    ewgeg

---
+++ Library/include/function.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_H_FR5RTZUZ9X
+#define FUNCTION_H_FR5RTZUZ9X
+
+int add(int a, int b);
+
+#endif
---
+++ Library/src/main.c
@@ -0,0 +1,12 @@
+#include "function.h"
+
+int main()
+{
+
+
+int val1 = 5;
+    int val2 = 6;
+    int ret_val = add(val1, val2);
+
+printf(%d,ret_val);
+}
